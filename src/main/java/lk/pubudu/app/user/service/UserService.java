28f8b22876db78ff9d24b14a3c8267c3eb0f7253
Pubudu Janith <pubudujanith123@gmail.com>
2023-10-14 17:12:55 +0530

    Implement temporary password generation logic

--- src/main/java/lk/pubudu/app/user/service/UserService.java
+++ src/main/java/lk/pubudu/app/user/service/UserService.java
@@ -5,11 +5,14 @@ import lk.pubudu.app.user.entity.User;
 import lk.pubudu.app.user.repository.UserRepository;
 import lk.pubudu.app.util.Transformer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,10 @@ public class UserService {
 
     @Transactional(rollbackFor = Throwable.class)
     public UserDTO createUser(UserDTO userDTO) {
+        Optional<User> availability = userRepository.findByEmail(userDTO.getEmail());
+        if (availability.isPresent()) throw new DuplicateKeyException("A user is already exists with this email id");
+        User incomingUser = transformer.toUserEntity(userDTO);
+
         return null;
     }
 
@@ -42,6 +49,30 @@ public class UserService {
             userDTOList.add(userDTO);
         }
         return userDTOList;
+    }
+
+    public String generateTemporaryPassword() {
+        String capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String simpleLetters = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String specialChars = "!@#$%^&*_=+-?<>";
+
+        String allChars = capitalLetters + simpleLetters + numbers + specialChars;
+        Random random = new Random();
+
+        int length = random.nextInt(7) + 8;
+        char[] password = new char[length];
+
+        password[0] = capitalLetters.charAt(random.nextInt(capitalLetters.length()));
+        password[1] = simpleLetters.charAt(random.nextInt(simpleLetters.length()));
+        password[2] = numbers.charAt(random.nextInt(numbers.length()));
+        password[3] = specialChars.charAt(random.nextInt(specialChars.length()));
+
+        for (int i = 4; i < length; i++) {
+            password[i] = allChars.charAt(random.nextInt(allChars.length()));
+        }
+
+        return new String(password);
     }
 
 }
