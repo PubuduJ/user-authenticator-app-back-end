ef22eb985c6f7300f7e14d39f4704ffb755829b1
Pubudu Janith <pubudujanith123@gmail.com>
2023-10-14 10:00:19 +0530

    Create RolePermission entity

--- src/main/java/lk/pubudu/app/role/entity/RolePermission.java
+++ src/main/java/lk/pubudu/app/role/entity/RolePermission.java
@@ -1,4 +1,24 @@
 package lk.pubudu.app.role.entity;
 
-public class RolePermission {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role_permission")
+public class RolePermission implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -2615437460171337653L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "permission_name", nullable = false, columnDefinition = "VARCHAR(100)")
+    private String permissionName;
 }
