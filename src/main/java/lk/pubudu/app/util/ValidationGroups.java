c2d63e9e3518036d26321f478ad8fa5fba0e22c8
Pubudu Janith <pubudujanith123@gmail.com>
2023-10-14 17:48:50 +0530

    Create validation groups

---
+++ src/main/java/lk/pubudu/app/util/ValidationGroups.java
@@ -0,0 +1,9 @@
+package lk.pubudu.app.util;
+
+import jakarta.validation.groups.Default;
+
+public interface ValidationGroups {
+
+    interface Create extends Default {}
+    interface Update extends Default {}
+}
