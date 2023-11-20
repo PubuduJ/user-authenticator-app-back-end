3667c77ee3b632cc840e151739d35be358710f9c
Pubudu Janith <pubudujanith123@gmail.com>
2023-11-20 10:54:27 +0530

    Import configuration classes to AppInitializer

--- src/main/java/lk/pubudu/app/AppInitializer.java
+++ src/main/java/lk/pubudu/app/AppInitializer.java
@@ -1,11 +1,15 @@
 package lk.pubudu.app;
 
+import lk.pubudu.app.config.ApplicationConfig;
+import lk.pubudu.app.config.SecurityConfiguration;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Import({ApplicationConfig.class, SecurityConfiguration.class})
 @EnableTransactionManagement
 @SpringBootApplication
 public class AppInitializer {
